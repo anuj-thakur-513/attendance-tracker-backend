a5c589cc2166e56e385e4ed2b662b120a27557ff
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-08-25 15:49:34 +0530

    feat: subject deletion route

--- src/controllers/subjectController.ts
+++ src/controllers/subjectController.ts
@@ -75,4 +75,23 @@ const handleGetAllSubjects = asyncHandler(
   }
 );
 
-export { handleAddSubject, handleGetAllSubjects };
+const handleDeleteSubject = asyncHandler(
+  async (req: Request, res: Response) => {
+    const subjectId = req.params.id;
+
+    const subject = await Subject.findById(subjectId);
+    if (!subject) {
+      throw new ApiError(404, "no subject found");
+    }
+
+    try {
+      await Subject.deleteOne({ _id: subjectId });
+    } catch (error) {
+      throw new ApiError(500, "error deleting the subject");
+    }
+
+    return res.status(200).json("subject deleted successfully");
+  }
+);
+
+export { handleAddSubject, handleGetAllSubjects, handleDeleteSubject };
--- src/routes/subjectRoutes.ts
+++ src/routes/subjectRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authMiddleware from "../middlewares/authMiddleware";
 import {
   handleAddSubject,
+  handleDeleteSubject,
   handleGetAllSubjects,
 } from "../controllers/subjectController";
 
@@ -9,5 +10,6 @@ const subjectRouter = Router();
 
 subjectRouter.post("/add", [authMiddleware, handleAddSubject]);
 subjectRouter.get("/all", [authMiddleware, handleGetAllSubjects]);
+subjectRouter.delete("/:id", [authMiddleware, handleDeleteSubject]);
 
 export default subjectRouter;
