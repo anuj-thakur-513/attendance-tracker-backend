caed7d2794b02ee041d539f261259103cef3ee12
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-08-26 22:56:58 +0530

    update CORS

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://myattendance-tracker.netlify.app/",
-    ],
+    origin: true,
     credentials: true,
   })
 );
