b1c3824885b5b10ad9a305592285a1cc70b0b13c
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-08-26 17:03:40 +0530

    remove redis

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -11,10 +11,4 @@ export default {
     clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
     clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
   },
-  redis: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT || 24952,
-    user: process.env.REDIS_USER,
-    password: process.env.REDIS_PASSWORD,
-  },
 };
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ dotenv.configDotenv({
 
 import app from "./server";
 import connectDB from "./services/db";
-import { redis } from "./services/redis";
 
 const PORT = process.env.PORT || 8000;
 
@@ -15,13 +14,6 @@ async function init() {
   } catch (error) {
     console.error(`Error connecting to DB: ${error}`);
   }
-
-  await redis.ping((err, res) => {
-    if (err) {
-      console.log(`error while connecting to redis:`, err);
-    }
-    console.log(`redis PING:${res}`);
-  });
 
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
